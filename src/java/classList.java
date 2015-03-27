43e294589d9651491c372e542be6d096540ad69b
kevin <kevin@kevin-PC>
2015-03-27 13:08:33 +0100

    Primer Commit

---
+++ src/java/classList.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin
+ */
+public class classList {
+    public String image0;
+    public String image1;
+    public String image2;
+    public String image3;
+    public String image4;
+    public String image5;
+    public String image6;
+    public String image7;    
+    
+    public classList(int a){
+    switch(a) {
+    case 0:
+        image0 = "imagenes/homer1.jpg";
+        image1 = "imagenes/homer2.jpg";
+        image2 = "imagenes/homer3.jpg";
+        image3 = "imagenes/homer4.jpg";
+        image4 = "imagenes/homer5.jpg";
+        image5 = "imagenes/homer6.jpg";
+        image6 = "imagenes/homer7.jpg";
+        image7 = "imagenes/homer8.jpg";
+        break;
+    case 1:
+        image0 = "imagenes/marge1.jpg";
+        image1 = "imagenes/marge2.jpg";
+        image2 = "imagenes/marge3.jpg";
+        image3 = "imagenes/marge4.jpg";
+        image4 = "imagenes/marge5.jpg";
+        image5 = "imagenes/marge6.jpg";
+        image6 = "imagenes/marge7.jpg";
+        image7 = "imagenes/marge8.jpg";
+        break;
+    case 2:
+        image0 = "imagenes/bart1.jpg";
+        image1 = "imagenes/bart2.jpg";
+        image2 = "imagenes/bart3.jpg";
+        image3 = "imagenes/bart4.jpg";
+        image4 = "imagenes/bart5.jpg";
+        image5 = "imagenes/bart6.jpg";
+        image6 = "imagenes/bart7.jpg";
+        image7 = "imagenes/bart8.jpg";
+        break;
+    case 3:
+        image0 = "imagenes/lisa1.jpg";
+        image1 = "imagenes/lisa2.jpg";
+        image2 = "imagenes/lisa3.jpg";
+        image3 = "imagenes/lisa4.jpg";
+        image4 = "imagenes/lisa5.jpg";
+        image5 = "imagenes/lisa6.jpg";
+        image6 = "imagenes/lisa7.jpg";
+        image7 = "imagenes/lisa8.jpg";
+        break;
+    case 4:
+        image0 = "imagenes/maggie1.jpg";
+        image1 = "imagenes/maggie2.jpg";
+        image2 = "imagenes/maggie3.jpg";
+        image3 = "imagenes/maggie4.jpg";
+        image4 = "imagenes/maggie5.jpg";
+        image5 = "imagenes/maggie6.jpg";
+        image6 = "imagenes/maggie7.jpg";
+        image7 = "imagenes/maggie8.jpg";
+        break;    
+    default:
+        image0 = "maggie1.jpg";
+        image1 = "maggie2.jpg";
+        image2 = "maggie3.jpg";
+        image3 = "maggie4.jpg";
+        image4 = "maggie5.jpg";
+        image5 = "maggie6.jpg";
+        image6 = "maggie7.jpg";
+        image7 = "maggie8.jpg";
+    }
+    }
+    
+}
---
+++ web/fichero.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function pulsado(operando){
+    //var contenidoA=document.getElementById(operando).value;
+    var image=document.getElementsByClassName('min');
+    for (contador=0;contador<image.length;contador++){
+        //spanA[contador].innerHTML=contenidoA;
+        //image[contador].defineProperty("src","imagenes/bart1.jpg");
+        image[contador].src = "imagenes/bart2.jpg";
+        
+    }
+
+}
+
